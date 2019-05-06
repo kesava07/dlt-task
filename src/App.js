f147b1f32eb67f1e29da413a455409c496bd466f
kesava07 <kesava07.ip@gmail.com>
2019-05-06 19:44:58 +0530

    complete

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Route, Switch, Redirect } from 'react-router-dom';
 import './App.css';
+import Login from './Containers/Auth/Login';
+import Registration from './Containers/Auth/Registration';
+import Dashboard from './Containers/Dashboard';
 
 function App() {
+  let user = localStorage.getItem("currentUser");
+  let routes = (
+    <Switch>
+      <Route exact path="/" component={Login} />
+      <Route exact path="/registration" component={Registration} />
+      <Redirect to="/" />
+    </Switch>
+  );
+  if (user) {
+    routes = (<Switch>
+      <Route exact path="/" component={Dashboard} />
+      <Route exact path="/dashboard" component={Dashboard} />
+      <Redirect to="/" />
+    </Switch>)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>{routes}</React.Fragment>
   );
 }
 
